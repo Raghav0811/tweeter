1173ff6771812fef170d7fcf210793c9ef7c28fd
vagrant <raghavbpdshrivastav@gmail.com>
2020-05-21 15:34:42 +0000

    adding images to images folder

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,13 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const renderTweets = function(tweets) {
+  if (Array.isArray(tweets)) {
+    return tweets.forEach(tweet => {
+      $('#tweets-container').prepend(createTweetElement(tweet));
+    });
+  } else {
+    return $('#tweets-container').prepend(createTweetElement(tweets));
+  }
+};
 
